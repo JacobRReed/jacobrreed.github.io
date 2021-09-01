b68aa41699e5472c5cd5e46a085d3c048c77fb7d
JacobReed <jacob.russell.reed@gmail.com>
2021-09-01 16:04:55 -0400

    Added images, init template with images

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { PageProps } from "gatsby";
+import "./index.scss";
+
+export default function IndexRoute(props: PageProps) {
+  return (
+    <main>
+      <h1>Under Construction</h1>
+    </main>
+  );
+}
